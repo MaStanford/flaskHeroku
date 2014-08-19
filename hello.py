2d375d5ffdbea08671aeeeb34d64c76693c87d5a
mstanford <marks@reliablecoders.com>
2014-08-18 23:28:34 -0700

    bananas

--- hello.py
+++ hello.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return '<img src="http://t1.gstatic.com/images?q=tbn:ANd9GcRMMh5MUMdgPHTFYS-RfCBzgmnRV5liQhKc1QScmt830x8UUMk00g"></img>'
