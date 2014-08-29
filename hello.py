2999120611ed61a6693f206794e968d6ff3c79f3
mstanford <marks@reliablecoders.com>
2014-08-28 18:24:11 -0700

    Dart is a stupid fucker.  It doesn't play well with flask static file server

--- hello.py
+++ hello.py
@@ -1,8 +1,33 @@
-import os
-from flask import Flask
+# all the imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from contextlib import closing
+
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
-@app.route('/')
-def hello():
-	return render_template(‘web/hellflask.html’)
+@app.route("/")
+def questionaire():
+	return render_template('helloflask.html')
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+if __name__ == "__main__":
+	init_db()
+	app.run()
